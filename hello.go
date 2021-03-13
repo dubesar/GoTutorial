bb945929c7c218ab2255605879b099194928d150
sarvesh <dubeysarvesh5525@gmail.com>
2021-03-13 21:39:00 +0530

    variables

--- hello.go
+++ hello.go
@@ -5,5 +5,13 @@ import (
 )
 
 func main() {
-	fmt.Println("Hello world")
+	var x int = 5
+	var y int = 10
+	fmt.Println(x + y)
+
+	//similarly
+	x1 := 5
+	y1 := 5
+	sum := x1 + y1
+	fmt.Println(sum)
 }

c8fe862b68970c5130fe8e58897b13f408175a50
Sarvesh Dubey <dubeysarvesh5525@gmail.com>
2021-08-26 13:16:16 +0530

    Update hello.go

--- hello.go
+++ hello.go
@@ -2,21 +2,50 @@ package main
 
 import (
 	"fmt"
+	"sort"
 )
 
+//searching element
+func indexOf(data []int, ele int) int {
+	for index, value := range data {
+		if ele == value {
+			return index
+		}
+	}
+	return -1
+}
+
 func main() {
+	//making dynamic array
 
-	//array of fixed size
-	var a [5]int
-	a[2] = 7
-	fmt.Println(a)
+	//initialize dynamic arr
+	arr := []int{}
+	arr = append(arr, 2, 3, 1, 100, 5)
 
-	//shorthand
-	b := [5]int{5, 4, 3, 2, 1}
-	fmt.Println(b)
+	fmt.Println(arr)
 
-	//dynamic array
-	c := []int{5, 4, 3}
-	c = append(c, 13)
-	fmt.Println(c)
+	//print a slice of array
+	fmt.Println(arr[0:2])
+
+	//making a fixed size array of size 4 with all initialized by 0
+	new_arr := make([]int, 4)
+	new_arr = append(new_arr, 5)
+
+	fmt.Println(new_arr)
+
+	//arr operations ->
+	//get an element at a particular location
+	fmt.Println(arr[2])
+
+	// sort an array
+	sort.Ints(arr)
+	fmt.Println(arr)
+
+	//remove an element
+	//by index
+	fmt.Println(append(arr[:1], arr[2:len(arr)-1]...))
+
+	//by value
+	fmt.Println(indexOf(arr, 3))
+	fmt.Println(append(arr[:indexOf(arr, 3)], arr[indexOf(arr, 3)+1:len(arr)-1]...))
 }

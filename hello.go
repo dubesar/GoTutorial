842c6008a338e0e35b314b8c63933ba5698c64d5
Sarvesh Dubey <dubeysarvesh5525@gmail.com>
2021-08-26 12:43:59 +0530

    Update hello.go

--- hello.go
+++ hello.go
@@ -5,22 +5,24 @@ import (
 )
 
 func main() {
-	for i := 0; i < 5; i++ {
-		fmt.Println(i)
-	}
-
-	//while loops
-	i := 0
-	for i < 5 {
-		fmt.Println(i)
-		i++
-	}
-
 	//looping arrays
 	arr := []string{"a", "b", "c"}
 	for index, value := range arr {
 		fmt.Println("index", index, "value", value)
 	}
-
 	//same goes for map just replace index with key
+	
+	//using for loops
+	sum := 0
+	for i := 0; i < 5; i++ {
+	    sum += i
+	}
+	fmt.Println(sum)
+
+	n := 25
+	for sum < 100 {
+	    sum += n
+	}
+
+	fmt.Println(sum)
 }

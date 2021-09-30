57bad69549ce0a09076d68d0dfa2d3b645836af2
Sarvesh <dubeysarvesh5525@gmail.com>
2021-10-01 00:26:28 +0530

    added functions

---
+++ functions.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+	"math"
+)
+
+func main() {
+	result := sum(2, 3)
+	fmt.Println(result)
+	res, err := sqrt(-16)
+	if err != nil {
+		fmt.Println(err)
+	} else {
+		fmt.Println(res)
+	}
+}
+
+func sum(x int, y int) int {
+	return x + y
+}
+
+func sqrt(x float64) (float64, error) {
+	if x < 0 {
+		return 0, errors.New("Undefined for negative numbers")
+	}
+	return math.Sqrt(x), nil
+}

3209e428c8aaed5acd11202bd26bd43be52497a6
Sarvesh <dubeysarvesh5525@gmail.com>
2021-10-01 00:25:56 +0530

    added changes

---
+++ loops.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	//looping arrays
+	arr := []string{"a", "b", "c"}
+	for index, value := range arr {
+		fmt.Println("index", index, "value", value)
+	}
+	//same goes for map just replace index with key
+
+	//using for loops
+	sum := 0
+	for i := 0; i < 5; i++ {
+		sum += i
+	}
+	fmt.Println(sum)
+
+	n := 25
+	for sum < 100 {
+		sum += n
+	}
+
+	fmt.Println(sum)
+}

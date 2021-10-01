e4d67cc5f38ade4b1aaba38b8388afd536170047
Tanish2002 <tanishkhare@gmail.com>
2021-10-01 21:19:27 +0530

    Add linear search

--- Algorithms/linear_search.go
+++ Algorithms/linear_search.go
@@ -1,5 +1,18 @@
 package main
 
+import "fmt"
+
+func linearsearch(array []int, key int) bool {
+	for _, item := range array {
+		if item == key {
+			return true
+		}
+	}
+	return false
+}
+
 func main() {
-	//implement linear search
+	items := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+	fmt.Println(linearsearch(items, 8))  // true
+	fmt.Println(linearsearch(items, 23)) // false
 }
